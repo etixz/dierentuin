1ce8dfb6800d61a70141385b02e949543e11670c
David <david.van.steertegem@gmail.com>
2019-12-05 14:39:34 +0100

    added queries in den DAO

--- src/main/java/eb/egonb/dierentuin/model/BeestDAO.java
+++ src/main/java/eb/egonb/dierentuin/model/BeestDAO.java
@@ -2,6 +2,9 @@ package eb.egonb.dierentuin.model;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BeestDAO extends CrudRepository<Beest, Integer> {
 
+    List<Beest> findByNaam(String naam);
 }
